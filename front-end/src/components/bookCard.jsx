a7590c526ada714e31f7f2433b9b4b7ad0bfc562
Kirito-C <149937627+Kirito-C@users.noreply.github.com>
2026-01-23 00:47:19 +0100

    Modificato il componente `bookCard` per migliorare la struttura e l'estetica dell'interfaccia utente. Aggiunto un'icona per rappresentare il libro e modificato la disposizione del contenuto per una visualizzazione più chiara e organizzata.

--- front-end/src/components/bookCard.jsx
+++ front-end/src/components/bookCard.jsx
@@ -1,7 +1,21 @@
 export default function bookCard({book, onDelete}) {
-    return {
+    return (
         <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow">
-            <div
+            <div className="card-body">
+                <div className="flex justify-between items-start">
+                    <span className="text-4xl">📖</span>
+                    <div className="badge badge-ghost badge-sm">
+                        #{book.id.slice(0, 8)}...
+                    </div>
+                </div>
+
+                <h2 className="card-title">{book.titolo}</h2>
+
+                <div className="space-y-1 text-sm">
+                    <p></p>
+
+                </div>
+            </div>
         </div>
-    }
+    )
 }
