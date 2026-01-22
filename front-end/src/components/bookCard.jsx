46a6ab9cbc55ca4182e86ad13016f40311208de8
Kirito-C <149937627+Kirito-C@users.noreply.github.com>
2026-01-23 00:44:09 +0100

    Aggiunto componente `bookCard.jsx` per visualizzare i dettagli dei libri.

---
+++ front-end/src/components/bookCard.jsx
@@ -0,0 +1,7 @@
+export default function bookCard({book, onDelete}) {
+    return {
+        <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow">
+            <div
+        </div>
+    }
+}
