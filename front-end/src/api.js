67d727d821f4fc66c9c41054a1c18d3be952d04f
Kirito-C <149937627+Kirito-C@users.noreply.github.com>
2026-01-23 00:12:39 +0100

    Aggiunto metodo GET esplicito e gestione errori per la funzione `getBooks`.

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -2,6 +2,12 @@ const API_URL = 'localhost:11000/api';
 
 export const api = {
     async getBooks(){
-        const request
+        // preferisco specificare che metodo è anche se la fetch di default usa metodo get
+        const response = fetch(`${API_URL}/libri`, {
+            method: 'GET'
+        });
+
+        if (!response.ok) throw new Error('Errore nel caricamento dei libri');
+        
     }
 }
