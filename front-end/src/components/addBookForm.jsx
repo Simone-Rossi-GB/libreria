18c5187c0d36fd06f6ef1952ec19d801d0ca9da1
Kirito-C <149937627+Kirito-C@users.noreply.github.com>
2026-01-23 01:00:15 +0100

    Convertito componente addBookForm in funzione e aggiunto gestione del stato per i dati del libro.

--- front-end/src/components/addBookForm.jsx
+++ front-end/src/components/addBookForm.jsx
@@ -1,3 +1,22 @@
 import { useState } from 'react';
 
-export default 
+export default function addBookForm({ onAdd }) {
+    const [formData, setFormData] = useState({
+        titolo: '',
+        autore: '',
+        anno: '',
+        genere: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        
+    }
+}
