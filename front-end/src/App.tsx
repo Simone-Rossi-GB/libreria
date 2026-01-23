455413049bc6c1a96ff263deeb17e889f5e584b4
Kirito-C <149937627+Kirito-C@users.noreply.github.com>
2026-01-23 01:31:51 +0100

    Modificato l'UI per mostrare un messaggio quando non ci sono risultati filtrati o libri presenti.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -103,15 +103,26 @@ function App() {
 
                 {/* spazio della UI per il libri. Note: filteredbooks contiene tutti i libri se non c'è nessun filtro */}
                 <div>
-                    <div>
-                        <svg>
-                            <path></path>
-                        </svg>
-                        <span>
-                            
-                        </span>
-                    </div>
+                    {filteredBooks.filter(book => (
+                        <B
+                    ))}
                 </div>
+
+                {filteredBooks.length === 0 && (
+                    <div>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current flex-shrink-0 w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                            </svg>
+                            <span>
+                                {books.length === 0
+                                    ? 'Nessun libro presente nella libreria. Aggiungi il primo libro'
+                                    : 'Nessun libro trovato con questi filtri'
+                                }
+                            </span>
+                        </div>
+                    </div>
+                )}
 
             </div>
         </div>
